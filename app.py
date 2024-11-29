10f2e3cfef6d72d7c43e5312c79bcd3658fc47ce
suzukaotto <me@szk.kr>
2024-11-29 05:03:21 +0000

    update: code update

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ HOST_IP = os.environ['HOST_IP']
 HOST_PORT = os.environ['HOST_PORT']
 PROCESS_URL = os.environ['PROCESS_URL']
 
-server_app = ServerApp(HOST_IP, HOST_PORT, True)
+server_app = ServerApp(HOST_IP, HOST_PORT, False)
 server_app.run()
